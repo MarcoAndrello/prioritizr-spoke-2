7fb248f97d6a6fcf6c06992f230ea18fa27b74f4
Marco Andrello <marco.andrello@gmail.com>
2023-12-15 14:16:13 +0100

    lavoro in ufficio 15 12 2023

--- 02 - Prioritization.R
+++ 02 - Prioritization.R
@@ -20,7 +20,7 @@ feature_names
 pus$unitary_cost <- 1
 
 # Limit to conservation features present in the pilot area
-features_present <- feature_names[which(local_area > 0)]
+features_present <- feature_names[which(local_area > 0)] # 398 species
 
 # p1: No cost
 p1 <-
@@ -53,20 +53,74 @@ p3 <-
 s3 <- solve(p3)
 plot(s3["solution_1"],border=NA,  main="p3: No cost + boundary penalties")
 eval_target_coverage_summary(p3,s3["solution_1"]) %>% print(n=400)
+# 
+# # p4: No cost + connectivity penalty
+# p4 <-
+#   problem(pus, features = features_present, cost_column="unitary_cost") %>%
+#   add_min_set_objective() %>%
+#   add_relative_targets(0.2) %>%
+#   add_boundary_penalties(0.001) %>%
+#   add_binary_decisions() %>%
+#   add_gurobi_solver(gap = 0.6, verbose = T)
+# s4 <- solve(p4)
+# plot(s4["solution_1"],border=NA,  main="p4: No cost + connectivity penalty")
 
-# p4: No cost + connectivity penalty
-p4 <-
+save(p1, s1,
+     p2, s2,
+     p3, s3,
+     file="Solutions_SoS_temp.RData")
+
+# p5: No cost + representation targets proportional to species distribution range
+# Rodrigues et al 2004 Bioscience
+# More demanding representation targets (a larger percentage of the range)
+# were set for species with more restricted ranges 
+relative_local_area <- local_area/nrow(pus) ## 635 species
+relative_targets<- c(1, 1, 1, 1, 0.9, 0.7, 0.5, 0.3, 0.1, 0.1)
+hist(relative_local_area)
+plot(cbind(seq(0.1,1,0.1),relative_targets),type="l", xlab="Distribution range (relative)", xlab="Representation target (relative)")
+relative_targets_species <- relative_targets[cut(relative_local_area,breaks=seq(0,1,0.1))]
+relative_targets_species <- relative_targets_species[which(local_area > 0)]
+
+p5l <-
   problem(pus, features = features_present, cost_column="unitary_cost") %>%
   add_min_set_objective() %>%
-  add_relative_targets(0.2) %>%
-  add_boundary_penalties(0.001) %>%
+  add_relative_targets(relative_targets_species) %>%
   add_binary_decisions() %>%
-  add_gurobi_solver(gap = 0.6, verbose = T)
-s4 <- solve(p4)
-plot(s4["solution_1"],border=NA,  main="p4: No cost + connectivity penalty")
+  add_gurobi_solver(gap = 0.1, verbose = T)
+s5l <- solve(p5l)
+eval_target_coverage_summary(p5l,s5l["solution_1"]) %>% print(n=400)
 
 
+relative_global_area <- med_area/27095 ## 635 species; 27095 is approx number of sea cells
+# Each cell of FishMed grid is 0.1 * 0.1 degree so approx 11 km * 6 km = 70 km2
+absolute_global_area = 70 * med_area 
+hist(absolute_global_area)
+### now find range area to target relationship 
+## there was a publication for setting targets
 
-# p5: No cost + representation targets proportional to species distributon range
+plot(cbind(seq(0.1,1,0.1),relative_targets),type="l", xlab="Distribution range (relative)", ylab="Representation target (relative)")
+relative_targets_species <- relative_targets[cut(relative_global_area,breaks=seq(0,1,0.1))]
+relative_targets_species <- relative_targets_species[which(local_area > 0)]
+
+p5g <-
+  problem(pus, features = features_present, cost_column="unitary_cost") %>%
+  add_min_set_objective() %>%
+  add_relative_targets(relative_targets_species) %>%
+  add_binary_decisions() %>%
+  add_gurobi_solver(gap = 0.1, verbose = T)
+s5g <- solve(p5g)
+eval_target_coverage_summary(p5g,s5g["solution_1"]) %>% print(n=400)
+
+save(p1, s1,
+     p2, s2,
+     p3, s3,
+     p5l, s5l,
+     file="Solutions_SoS_temp.RData")
+
+###
+
 # p6: like p1 but maximal coverage
+
+
+
 # p7: like p1 but locking in existing PAs
